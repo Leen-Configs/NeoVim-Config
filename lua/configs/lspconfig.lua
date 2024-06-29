5f4b4a417b31055673c51bf68a29c526e815b2f8
DrunkLeen <drunkleen@gmail.com>
2024-06-29 19:36:55 +0200

    upload stage

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,41 @@
+-- EXAMPLE 
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "html", "cssls", "gopls", "jedi_language_server" }
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
+end
+
+-- typescript
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+-- Go language server
+lspconfig.gopls.setup {
+  on_attach = custom_on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = {"go", "gomod", "gowork", "gotmpl"},
+  -- root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+      gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparama = true,
+      }
+    }
+  }
+}
